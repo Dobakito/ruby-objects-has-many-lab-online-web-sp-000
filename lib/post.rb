be09efffba11c4bea4690df06a21a7bc3d5df508
Michael Dobak <mdobak44@icloud.com>
2021-02-26 15:49:02 +0000

    Automatically backed up by Learn

---
+++ lib/post.rb
@@ -0,0 +1,16 @@
+class Post 
+  attr_accessor :title, :author
+  @@all = []
+  def initialize(title)
+    @@all << self
+    @title = title
+  end 
+  
+  def self.all
+    @@all 
+  end 
+  
+  def author_name
+    self.author.name
+  end
+end 
